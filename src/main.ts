cf48ce9dbb3e383bce8c0a599a02e050c75117ee
Oguzhan <oguzhanbaysal@outlook.com>
2025-01-26 00:47:14 +0300

    third commit

--- src/main.ts
+++ src/main.ts
@@ -12,6 +12,6 @@ async function bootstrap() {
     allowedHeaders: ['Content-Type', 'Authorization']
   });
 
-  await app.listen(3001);
+  await app.listen(process.env.PORT || 3001);
 }
 bootstrap(); 
