f2c30b23f0f1bab4b6613e9607a48749600b842d
Oguzhan <oguzhanbaysal@outlook.com>
2025-01-26 03:24:01 +0300

    fourth commit

--- src/main.ts
+++ src/main.ts
@@ -4,14 +4,18 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
-  // CORS ayarları
+  // CORS ayarları güncellendi
   app.enableCors({
-    origin: 'http://localhost:3000', // Next.js client'ın adresi
+    origin: [
+      'https://lumflights-client.vercel.app',
+      'http://localhost:3000'
+    ],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     credentials: true,
     allowedHeaders: ['Content-Type', 'Authorization']
   });
 
+  // Port ayarı process.env.PORT'tan gelecek şekilde güncellendi
   await app.listen(process.env.PORT || 3001);
 }
 bootstrap(); 
